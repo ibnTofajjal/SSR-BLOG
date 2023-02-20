1800c5d5019ec1a42913e80ab413485ff809aeaa
Joynal Bin Tofajjal <joynal.bintofajjal@gmail.com>
2023-02-21 04:28:50 +0600

    Create a Post without Multer

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -7,4 +7,10 @@ router.get("/add-blog", (req, res) => {
   return res.render("addBlog", { user: req.user });
 });
 
+// POST BLOG
+router.post("/", (req, res) => {
+  console.log(req.body);
+  return res.redirect("/");
+});
+
 module.exports = router;
